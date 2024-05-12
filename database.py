fad5615a7a4b9b8888cf74a779b40956e76afbb2
kalashsingh18 <kalashchouhan939@gmail.com>
2024-05-13 01:15:30 +0530

    all the things updated on the new databases

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-database_url="postgresql+psycopg2://ems_api_database_user:yfWBbypt3Grdl2dXLH3G3hwJaIAQhKkR@dpg-col8sdgcmk4c73bnvq3g-a.oregon-postgres.render.com/ems_api_database"
+
+database_url="postgresql+psycopg2://ems_api_database_p0cy_user:xXVXa3z4Zu9x2dh0dAuyZDkWqQ3nBgzv@dpg-cp0hngg21fec73855e9g-a/ems_api_database_p0cy"
 engine=create_engine(database_url)
 session_local=sessionmaker(autoflush=False,autocommit=False,bind=engine)
 base=declarative_base()
